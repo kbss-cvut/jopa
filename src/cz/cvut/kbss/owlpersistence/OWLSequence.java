e3cb3cccac3d69f487366ff4f95d2fedcf53eadf
kremen <kremen@04d0684d-304b-4faa-bdc5-c409a98f0c25>
2009-06-14 08:34:16 +0000

    support for simple and referenced sequences.

---
+++ src/cz/cvut/kbss/owlpersistence/OWLSequence.java
@@ -0,0 +1,39 @@
+package cz.cvut.kbss.owlpersistence;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import cz.cvut.kbss.owlpersistence.owlapi.SequencesVocabulary;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface OWLSequence {
+
+	/**
+	 * Defines the type of the sequence.
+	 */
+	OWLSequenceType type() default OWLSequenceType.referenced;
+
+	/**
+	 * URI of the class that represents the 'OWLList' concept.
+	 * 
+	 * Relevant only for REFERENCED type.
+	 */
+	String ClassOWLListURI() default SequencesVocabulary.s_c_OWLList;
+
+	/**
+	 * URI of the object property that represents the 'hasContents' role.
+	 *
+	 * Relevant only for REFERENCED type.
+	 */
+	String ObjectPropertyHasContentsURI() default SequencesVocabulary.s_p_hasContents;
+
+	/**
+	 * URI of the object property that represents the 'hasNext' role.
+	 */
+	String ObjectPropertyHasNextURI() default SequencesVocabulary.s_p_hasNext;
+}
---
+++ src/cz/cvut/kbss/owlpersistence/OWLSequenceType.java
@@ -0,0 +1,29 @@
+package cz.cvut.kbss.owlpersistence;
+
+/**
+ * Defines types of sequence modeling in OWL.
+ * 
+ * @author kremen
+ */
+public enum OWLSequenceType {
+	/**
+	 * Used for simple (nonreferenced) sequences.
+	 * 
+	 * This means that elements of the sequence are unique to the sequence owner
+	 * and are NOT shared with other sequences.
+	 * 
+	 * TODO example
+	 */
+	simple,
+
+	/**
+	 * Used for referenced sequences. This case is more general, but sequence
+	 * representation requires more space (linear in the original size)
+	 * 
+	 * This means that elements of the sequence are not unique to the sequence
+	 * owner. Thus these elements might be referenced by other sequences.
+	 * 
+	 * TODO example
+	 */
+	referenced;
+}
