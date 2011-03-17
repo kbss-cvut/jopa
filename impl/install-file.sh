782d960668666fd15884ceb536f30c39e0ae6585
kremen <kremen@04d0684d-304b-4faa-bdc5-c409a98f0c25>
2011-03-17 20:19:58 +0000

    pom files

---
+++ impl/install-file.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=lib/owlapi/owlapi-bin.jar -DartifactId=owlapi -DgroupId=owl.semanticweb -DgeneratePom -Dpackaging=jar -Dsources=lib/owlapi/owlapi-src.jar -Dversion=3.2.2
