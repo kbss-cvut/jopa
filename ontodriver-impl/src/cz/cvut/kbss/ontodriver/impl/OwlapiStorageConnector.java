62b91cb8ba9b3815b46de6ae63baefe88b62c909
ledvima1 <ledvima1@04d0684d-304b-4faa-bdc5-c409a98f0c25>
2013-02-21 12:19:17 +0000

    Working on connectors.

---
+++ ontodriver-impl/src/cz/cvut/kbss/ontodriver/impl/OwlapiStorageConnector.java
@@ -0,0 +1,24 @@
+package cz.cvut.kbss.ontodriver.impl;
+
+import cz.cvut.kbss.ontodriver.OntoDriverException;
+import cz.cvut.kbss.ontodriver.StorageConnector;
+
+public class OwlapiStorageConnector implements StorageConnector {
+
+	public OwlapiStorageConnector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void close() throws OntoDriverException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isOpen() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
