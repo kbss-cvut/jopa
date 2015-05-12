564956f106c6a8205e2734b89b5485afe3b5d497
ledvima1 <ledvima1@04d0684d-304b-4faa-bdc5-c409a98f0c25>
2015-05-12 10:14:37 +0000

    Added tests in Sesame integration which were accidentally removed in previous commit.

---
+++ jopa-integration-tests-sesame/src/test/java/cz/cvut/kbss/jopa/test/integration/sesame/TestRetrieveOperationsNativeStore.java
@@ -0,0 +1,113 @@
+package cz.cvut.kbss.jopa.test.integration.sesame;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.jopa.owlapi.OWLAPIPersistenceProperties;
+import cz.cvut.kbss.jopa.test.environment.SesameNativeStorageConfig;
+import cz.cvut.kbss.jopa.test.environment.StorageConfig;
+import cz.cvut.kbss.jopa.test.environment.TestEnvironment;
+import cz.cvut.kbss.jopa.test.runners.RetrieveOperationsRunner;
+import cz.cvut.kbss.ontodriver.OntoDriverProperties;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class TestRetrieveOperationsNativeStore {
+
+	private static final Logger LOG = Logger.getLogger(TestRetrieveOperationsNativeStore.class
+			.getName());
+
+	private static final StorageConfig storage = initStorage();
+	private static final Map<String, String> properties = initProperties();
+
+	private RetrieveOperationsRunner runner;
+
+	private EntityManager em;
+
+	@Before
+	public void setUp() {
+		this.runner = new RetrieveOperationsRunner(LOG);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (em.isOpen()) {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+			em.getEntityManagerFactory().close();
+		}
+	}
+
+	@Test
+	public void testRetrieveSimple() {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveSimple", storage, false,
+				properties);
+		runner.retrieveSimple(em, context());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testRetrieveNull() {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveNull", storage, false,
+				properties);
+		runner.retrieveNull(em, context());
+	}
+
+	@Test
+	public void testRetrieveLazy() throws Exception {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveLazy", storage, false,
+				properties);
+		runner.retrieveLazily(em, context());
+	}
+
+	@Test
+	public void testRetrieveGenerated() throws Exception {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveGenerated", storage,
+				false, properties);
+		runner.retrieveGenerated(em, context());
+	}
+
+	@Test
+	public void testRetrieveNotExisting() {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveNotExisting", storage,
+				false, properties);
+		runner.retrieveNotExisting(em, context());
+	}
+
+	@Test
+	public void testRefresh() {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRefresh", storage, false,
+				properties);
+		runner.refresh(em, context());
+	}
+	
+	@Test
+	public void testRetrieveDifferentType() {
+		em = TestEnvironment.getPersistenceConnector("SesameNativeRetrieveDiffentType", storage, false,
+				properties);
+		runner.retrieveDifferentType(em, context());
+	}
+
+	private URI context() {
+		// Don't use contexts for now
+		return null;
+	}
+
+	private static StorageConfig initStorage() {
+		return new SesameNativeStorageConfig();
+	}
+
+	private static Map<String, String> initProperties() {
+		final Map<String, String> map = new HashMap<>();
+		map.put(OntoDriverProperties.USE_TRANSACTIONAL_ONTOLOGY, Boolean.TRUE.toString());
+		map.put(OntoDriverProperties.SESAME_USE_INFERENCE, Boolean.FALSE.toString());
+		map.put(OWLAPIPersistenceProperties.LANG, "en");
+		map.put("storage", "new");
+		return map;
+	}
+}
