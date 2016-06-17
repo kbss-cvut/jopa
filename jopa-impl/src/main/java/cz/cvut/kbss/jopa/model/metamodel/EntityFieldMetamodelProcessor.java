c2e14a64e1481487af284d7484dda85315594ee4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-06-17 18:28:38 +0200

    Do not log static field skipping in metamodel building, it floods the log output with AspectJ injected fields.

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/model/metamodel/EntityFieldMetamodelProcessor.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/model/metamodel/EntityFieldMetamodelProcessor.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2016 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.model.metamodel;
 
@@ -30,9 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * @author ledvima1
- */
 public class EntityFieldMetamodelProcessor<X> {
 
     private static final Logger LOG = LoggerFactory.getLogger(EntityFieldMetamodelProcessor.class);
@@ -52,7 +47,10 @@ public class EntityFieldMetamodelProcessor<X> {
     public void processField(Field field) {
         LOG.trace("processing field: {}", field);
         if (EntityPropertiesUtils.isFieldTransient(field)) {
-            LOG.trace("Skipping transient field {}", field);
+            // Do not log static fields
+            if (!EntityPropertiesUtils.isFieldStatic(field)) {
+                LOG.trace("Skipping transient field {}", field);
+            }
             return;
         }
         if (field.getType().isPrimitive()) {
