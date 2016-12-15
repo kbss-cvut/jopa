6f9e3447a7032d23b9e0f66d07cd99a549b9c574
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-12-15 14:16:22 +0100

    Swallow NoSuchMethodException in instance builder, there is no point logging it because it is likely to be pretty common (e.g. for singleton collections).

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/sessions/AbstractInstanceBuilder.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/sessions/AbstractInstanceBuilder.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (C) 2016 Czech Technical University in Prague
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any
  * later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -92,7 +92,7 @@ abstract class AbstractInstanceBuilder {
                 c.setAccessible(true);
             }
         } catch (NoSuchMethodException e) {
-            LOG.trace("Constructor in type {} taking arguments {} not found. Exception: {}", javaClass, args, e);
+            // No constructor matching the argument types
             return null;
         }
         return c;
