6280e02a305877ebf71db1682d2519d3d26c6623
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2017-02-02 14:49:17 +0100

    Added annotations for entity lifecycle listeners.

---
+++ jopa-api/src/main/java/cz/cvut/kbss/jopa/model/annotations/PostRemove.java
@@ -0,0 +1,15 @@
+package cz.cvut.kbss.jopa.model.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies a callback method for the corresponding lifecycle event. This annotation may be applied to methods of an
+ * entity class, a mapped superclass, or a callback listener class.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PostRemove {
+}
