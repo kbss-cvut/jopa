ea469ade20d1e26b7b668ac33b213289b7554b53
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-02-08 13:22:07 +0100

    JenaDriver - enabled delete integration tests.

---
+++ jopa-integration-tests-jena/src/test/java/cz/cvut/kbss/jopa/test/integration/jena/DeleteOperationsMultiContextTest.java
@@ -0,0 +1,42 @@
+package cz.cvut.kbss.jopa.test.integration.jena;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.jopa.test.environment.JenaDataAccessor;
+import cz.cvut.kbss.jopa.test.environment.JenaPersistenceFactory;
+import cz.cvut.kbss.jopa.test.environment.Triple;
+import cz.cvut.kbss.jopa.test.runner.DeleteOperationsMultiContextRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class DeleteOperationsMultiContextTest extends DeleteOperationsMultiContextRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DeleteOperationsMultiContextTest.class);
+
+    private final JenaPersistenceFactory persistenceFactory;
+    private final JenaDataAccessor dataAccessor;
+
+    public DeleteOperationsMultiContextTest() {
+        super(LOG);
+        this.persistenceFactory = new JenaPersistenceFactory();
+        this.dataAccessor = new JenaDataAccessor();
+    }
+
+    @Override
+    protected EntityManager getEntityManager(String repositoryName, boolean cacheEnabled,
+                                             Map<String, String> properties) {
+        return persistenceFactory.getEntityManager(repositoryName, cacheEnabled, properties);
+    }
+
+    @Override
+    protected void persistTestData(Collection<Triple> data, EntityManager em) {
+        dataAccessor.persistTestData(data, em);
+    }
+
+    @Override
+    protected void verifyStatementsPresent(Collection<Triple> expected, EntityManager em) {
+        dataAccessor.verifyDataPresence(expected, em);
+    }
+}
