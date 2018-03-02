a983f9d1ecc7e38fb0fce9441a53f04a65865f97
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-03-02 17:53:32 +0100

    [Enhancement #18] Simple list persist.

--- ontodriver-jena/src/main/java/cz/cvut/kbss/ontodriver/jena/list/SimpleListHandler.java
+++ ontodriver-jena/src/main/java/cz/cvut/kbss/ontodriver/jena/list/SimpleListHandler.java
@@ -8,9 +8,12 @@ import cz.cvut.kbss.ontodriver.model.NamedResource;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 
 class SimpleListHandler extends ListHandler<SimpleListDescriptor, SimpleListValueDescriptor> {
 
@@ -21,7 +24,7 @@ class SimpleListHandler extends ListHandler<SimpleListDescriptor, SimpleListValu
     @Override
     public List<Axiom<NamedResource>> loadList(SimpleListDescriptor descriptor) {
         final List<Axiom<NamedResource>> result = new ArrayList<>();
-        final Resource owner = ResourceFactory.createResource(descriptor.getListOwner().getIdentifier().toString());
+        final Resource owner = createResource(descriptor.getListOwner().getIdentifier().toString());
         final Property hasList = ResourceFactory
                 .createProperty(descriptor.getListProperty().getIdentifier().toString());
         final Property hasNext = ResourceFactory.createProperty(descriptor.getNextNode().getIdentifier().toString());
@@ -35,7 +38,35 @@ class SimpleListHandler extends ListHandler<SimpleListDescriptor, SimpleListValu
 
     @Override
     void persistList(SimpleListValueDescriptor descriptor) {
+        if (descriptor.getValues().isEmpty()) {
+            return;
+        }
+        if (descriptor.getContext() != null) {
+            connector.add(axiomsToStatements(descriptor), descriptor.getContext().toString());
+        } else {
+            connector.add(axiomsToStatements(descriptor));
+        }
+    }
 
+    private List<Statement> axiomsToStatements(SimpleListValueDescriptor descriptor) {
+        final List<Statement> statements = new ArrayList<>(descriptor.getValues().size());
+        final Resource owner = createResource(descriptor.getListOwner().getIdentifier().toString());
+        final Property hasList = ResourceFactory
+                .createProperty(descriptor.getListProperty().getIdentifier().toString());
+        final Property hasNext = ResourceFactory.createProperty(descriptor.getNextNode().getIdentifier().toString());
+        Resource previous = owner;
+        boolean first = true;
+        for (NamedResource value : descriptor.getValues()) {
+            final Resource node = createResource(value.getIdentifier().toString());
+            if (first) {
+                statements.add(ResourceFactory.createStatement(previous, hasList, node));
+                first = false;
+            } else {
+                statements.add(ResourceFactory.createStatement(previous, hasNext, node));
+            }
+            previous = node;
+        }
+        return statements;
     }
 
     @Override
--- ontodriver-jena/src/test/java/cz/cvut/kbss/ontodriver/jena/list/SimpleListHandlerTest.java
+++ ontodriver-jena/src/test/java/cz/cvut/kbss/ontodriver/jena/list/SimpleListHandlerTest.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.ontodriver.jena.list;
 
 import cz.cvut.kbss.ontodriver.descriptor.SimpleListDescriptor;
 import cz.cvut.kbss.ontodriver.descriptor.SimpleListDescriptorImpl;
+import cz.cvut.kbss.ontodriver.descriptor.SimpleListValueDescriptor;
 import cz.cvut.kbss.ontodriver.jena.connector.StorageConnector;
 import cz.cvut.kbss.ontodriver.jena.environment.Generator;
 import cz.cvut.kbss.ontodriver.model.Assertion;
@@ -9,18 +10,23 @@ import cz.cvut.kbss.ontodriver.model.Axiom;
 import cz.cvut.kbss.ontodriver.model.NamedResource;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
-import static org.apache.jena.rdf.model.ResourceFactory.createResource;
+import static org.apache.jena.rdf.model.ResourceFactory.*;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 public class SimpleListHandlerTest {
 
@@ -70,5 +76,48 @@ public class SimpleListHandlerTest {
         final List<URI> actual = result.stream().map(ax -> ax.getValue().getValue().getIdentifier())
                                        .collect(Collectors.toList());
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void persistListInsertsStatementsCorrespondingToList() {
+        final List<URI> list = IntStream.range(0, 5).mapToObj(i -> Generator.generateUri())
+                                        .collect(Collectors.toList());
+        final SimpleListValueDescriptor descriptor = new SimpleListValueDescriptor(RESOURCE, HAS_LIST, HAS_NEXT);
+        list.forEach(item -> descriptor.addValue(NamedResource.create(item)));
+        handler.persistList(descriptor);
+        final List<Statement> expected = getExpectedStatementsForPersist(list);
+        verify(connectorMock).add(expected);
+    }
+
+    private List<Statement> getExpectedStatementsForPersist(List<URI> list) {
+        final List<Statement> expected = new ArrayList<>(list.size());
+        expected.add(createStatement(createResource(RESOURCE.getIdentifier().toString()),
+                createProperty(HAS_LIST.getIdentifier().toString()), createResource(list.get(0).toString())));
+        final Property hasNext = createProperty(HAS_NEXT.getIdentifier().toString());
+        for (int i = 0; i < list.size() - 1; i++) {
+            expected.add(createStatement(createResource(list.get(i).toString()), hasNext,
+                    createResource(list.get(i + 1).toString())));
+        }
+        return expected;
+    }
+
+    @Test
+    public void persistListDoesNothingForEmptyDescriptor() {
+        final SimpleListValueDescriptor descriptor = new SimpleListValueDescriptor(RESOURCE, HAS_LIST, HAS_NEXT);
+        handler.persistList(descriptor);
+        verify(connectorMock, never()).add(anyListOf(Statement.class));
+    }
+
+    @Test
+    public void persistListWithContextInsertsStatementsCorrespondingToListIntoContext() {
+        final List<URI> list = IntStream.range(0, 5).mapToObj(i -> Generator.generateUri())
+                                        .collect(Collectors.toList());
+        final SimpleListValueDescriptor descriptor = new SimpleListValueDescriptor(RESOURCE, HAS_LIST, HAS_NEXT);
+        list.forEach(item -> descriptor.addValue(NamedResource.create(item)));
+        final URI context = Generator.generateUri();
+        descriptor.setContext(context);
+        handler.persistList(descriptor);
+        final List<Statement> expected = getExpectedStatementsForPersist(list);
+        verify(connectorMock).add(expected, context.toString());
     }
 }
