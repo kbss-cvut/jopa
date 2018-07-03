b21ebf13d64c9d9f72d03c08bf2cedfd73e0543e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-03 13:33:56 +0200

    Added classes of constants with vocabularies for OWL, RDF, RDFS and XSD. Deprecated CommonVocabulary.

--- jopa-api/src/main/java/cz/cvut/kbss/jopa/CommonVocabulary.java
+++ jopa-api/src/main/java/cz/cvut/kbss/jopa/CommonVocabulary.java
@@ -1,19 +1,22 @@
 /**
  * Copyright (C) 2016 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa;
 
+/**
+ * @deprecated Use {@link cz.cvut.kbss.jopa.vocabulary.OWL}, {@link cz.cvut.kbss.jopa.vocabulary.RDF}, {@link
+ * cz.cvut.kbss.jopa.vocabulary.RDFS}, {@link cz.cvut.kbss.jopa.vocabulary.XSD}
+ */
+@Deprecated
 public class CommonVocabulary {
 
     public static final String RDF_NAMESPACE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
@@ -21,6 +24,8 @@ public class CommonVocabulary {
     public static final String XSD_NAMESPACE = "http://www.w3.org/2001/XMLSchema#";
 
     public static final String RDFS_LABEL = RDFS_NAMESPACE + "label";
+
+    public static final String RDFS_COMMENT = RDFS_NAMESPACE + "comment";
 
     public static final String DC_DESCRIPTION = "http://purl.org/dc/elements/1.1/description";
 
---
+++ jopa-api/src/main/java/cz/cvut/kbss/jopa/vocabulary/RDF.java
@@ -0,0 +1,56 @@
+package cz.cvut.kbss.jopa.vocabulary;
+
+/**
+ * A subset of the RDF vocabulary.
+ */
+public class RDF {
+
+    /**
+     * RDF vocabulary namespace.
+     */
+    public static final String NAMESPACE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+
+    /**
+     * The {@code rdf:type} property indicates that a resource is a member of a class.
+     */
+    public static final String TYPE = NAMESPACE + "type";
+
+    /**
+     * The {@code rdf:value} property identifies the principal value (usually a string) of a property when the property
+     * value is a structured resource.
+     */
+    public static final String VALUE = NAMESPACE + "value";
+
+    /**
+     * The {@code rdf:Statement} class represents statements about the properties of resources.
+     *
+     * @see #SUBJECT
+     * @see #PREDICATE
+     * @see #OBJECT
+     */
+    public static final String STATEMENT = NAMESPACE + "Statement";
+
+    /**
+     * The subject of an RDF statement.
+     */
+    public static final String SUBJECT = NAMESPACE + "subject";
+
+    /**
+     * The predicate of an RDF statement.
+     */
+    public static final String PREDICATE = NAMESPACE + "predicate";
+
+    /**
+     * The predicate of an RDF statement.
+     */
+    public static final String OBJECT = NAMESPACE + "object";
+
+    /**
+     * {@code rdf:Property} represents those resources that are RDF properties.
+     */
+    public static final String PROPERTY = NAMESPACE + "property";
+
+    private RDF() {
+        throw new AssertionError();
+    }
+}
