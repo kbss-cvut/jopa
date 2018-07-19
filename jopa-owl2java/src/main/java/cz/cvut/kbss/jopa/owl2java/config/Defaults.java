d6f30f1766841e15efa43649ecb7094dba925ee8
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-19 10:29:35 +0200

    Added forgotten final modifier to Defaults constants in OWL2Java.

--- jopa-owl2java/src/main/java/cz/cvut/kbss/jopa/owl2java/config/Defaults.java
+++ jopa-owl2java/src/main/java/cz/cvut/kbss/jopa/owl2java/config/Defaults.java
@@ -12,42 +12,42 @@ public class Defaults {
     /**
      * @see Option#WITH_IRIS
      */
-    public static boolean WITH_IRIS = false;
+    public static final boolean WITH_IRIS = false;
 
     /**
      * @see Option#TARGET_DIR
      */
-    public static String TARGET_DIR = "";
+    public static final String TARGET_DIR = "";
 
     /**
      * @see Option#PACKAGE
      */
-    public static String PACKAGE = "generated";
+    public static final String PACKAGE = "generated";
 
     /**
      * @see Option#WHOLE_ONTOLOGY_AS_IC
      */
-    public static boolean WHOLE_ONTOLOGY_AS_IC = false;
+    public static final boolean WHOLE_ONTOLOGY_AS_IC = false;
 
     /**
      * @see Option#IGNORE_FAILED_IMPORTS
      */
-    public static boolean IGNORE_FAILED_IMPORTS = false;
+    public static final boolean IGNORE_FAILED_IMPORTS = false;
 
     /**
      * @see Option#JAVA_CLASSNAME_ANNOTATION
      */
-    public static String JAVA_CLASSNAME_ANNOTATION = Constants.P_CLASS_NAME;
+    public static final String JAVA_CLASSNAME_ANNOTATION = Constants.P_CLASS_NAME;
 
     /**
      * @see Option#PROPERTIES_TYPE
      */
-    public static String PROPERTIES_TYPE = PropertiesType.string.name();
+    public static final String PROPERTIES_TYPE = PropertiesType.string.name();
 
     /**
      * @see Option#GENERATE_JAVADOC_FROM_COMMENT
      */
-    public static boolean GENERATE_JAVADOC_FROM_COMMENT = true;
+    public static final boolean GENERATE_JAVADOC_FROM_COMMENT = true;
 
     private Defaults() {
         throw new AssertionError();
