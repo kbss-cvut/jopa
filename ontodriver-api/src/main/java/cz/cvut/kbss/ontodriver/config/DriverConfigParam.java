756bed38278c592fd33d92c5e6e0e2839e65e3a2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-07-11 10:17:56 +0200

    [Deprecation] Deprecate language setting for OntoDriver. Language is set on PU-level and the driver uses language specified by assertions.

--- ontodriver-api/src/main/java/cz/cvut/kbss/ontodriver/config/DriverConfigParam.java
+++ ontodriver-api/src/main/java/cz/cvut/kbss/ontodriver/config/DriverConfigParam.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.config;
 
@@ -23,6 +21,11 @@ public enum DriverConfigParam implements ConfigurationParameter {
 
     AUTO_COMMIT(OntoDriverProperties.CONNECTION_AUTO_COMMIT),
     REASONER_FACTORY_CLASS(OntoDriverProperties.REASONER_FACTORY_CLASS),
+    /**
+     * @deprecated Ontology language should be set at persistence unit level. The driver uses language specified per
+     * {@link cz.cvut.kbss.ontodriver.model.Assertion}.
+     */
+    @Deprecated
     ONTOLOGY_LANGUAGE(OntoDriverProperties.ONTOLOGY_LANGUAGE),
     USE_TRANSACTIONAL_ONTOLOGY(OntoDriverProperties.USE_TRANSACTIONAL_ONTOLOGY),
     MODULE_EXTRACTION_SIGNATURE(OntoDriverProperties.MODULE_EXTRACTION_SIGNATURE);
--- ontodriver-api/src/test/java/cz/cvut/kbss/ontodriver/config/DriverConfigurationTest.java
+++ ontodriver-api/src/test/java/cz/cvut/kbss/ontodriver/config/DriverConfigurationTest.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.config;
 
@@ -33,22 +31,21 @@ public class DriverConfigurationTest {
     public void addConfigurationFiltersKnownConfigurationProperties() {
         final Map<String, String> props = new HashMap<>();
         props.put(OntoDriverProperties.CONNECTION_AUTO_COMMIT, "true");
-        props.put(OntoDriverProperties.ONTOLOGY_LANGUAGE, "en");
         props.put(OntoDriverProperties.USE_TRANSACTIONAL_ONTOLOGY, "false");
 
         configuration.addConfiguration(props,
-                Arrays.asList(DriverConfigParam.AUTO_COMMIT, DriverConfigParam.MODULE_EXTRACTION_SIGNATURE,
+                Arrays.asList(DriverConfigParam.MODULE_EXTRACTION_SIGNATURE,
                         DriverConfigParam.USE_TRANSACTIONAL_ONTOLOGY));
-        assertNotNull(configuration.getProperty(DriverConfigParam.AUTO_COMMIT));
+        assertNull(configuration.getProperty(DriverConfigParam.AUTO_COMMIT)); // Not in the known properties
         assertNotNull(configuration.getProperty(DriverConfigParam.USE_TRANSACTIONAL_ONTOLOGY));
-        assertNull(configuration.getProperty(DriverConfigParam.ONTOLOGY_LANGUAGE));   // Not in the known properties
     }
 
     @Test
     public void getWithDefaultReturnsDefaultWhenConfigParamValueIsNotFound() {
-        assertNull(configuration.getProperty(DriverConfigParam.ONTOLOGY_LANGUAGE));
-        final String defaultLang = "en";
-        assertEquals(defaultLang, configuration.getProperty(DriverConfigParam.ONTOLOGY_LANGUAGE, defaultLang));
+        assertNull(configuration.getProperty(DriverConfigParam.USE_TRANSACTIONAL_ONTOLOGY));
+        final String defaultValue = "false";
+        assertEquals(defaultValue,
+                configuration.getProperty(DriverConfigParam.USE_TRANSACTIONAL_ONTOLOGY, defaultValue));
     }
 
     @Test
