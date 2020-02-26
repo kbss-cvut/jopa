97926b5d22a3372286ef932252e6dc7080ebb9ec
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-02-26 18:08:33 +0100

    [Task #42] Rewrite some of the tests to JUnit 5.

--- jopa-impl/src/test/java/cz/cvut/kbss/jopa/accessors/DefaultStorageAccessorTest.java
+++ jopa-impl/src/test/java/cz/cvut/kbss/jopa/accessors/DefaultStorageAccessorTest.java
@@ -1,49 +1,42 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.accessors;
 
 import cz.cvut.kbss.jopa.exceptions.OWLPersistenceException;
 import cz.cvut.kbss.jopa.exceptions.StorageAccessException;
 import cz.cvut.kbss.ontodriver.OntologyStorageProperties;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.util.Collections;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class DefaultStorageAccessorTest {
+class DefaultStorageAccessorTest {
 
     private static final String DATA_SOURCE_CLASS = DataSourceStub.class.getName();
     private static final String INVALID_DATA_SOURCE_CLASS = InvalidDataSource.class.getName();
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void createsDataSourceFromClassName() {
+    void createsDataSourceFromClassName() {
         final DefaultStorageAccessor a =
                 new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS), Collections.emptyMap());
         assertNotNull(a);
@@ -56,22 +49,23 @@ public class DefaultStorageAccessorTest {
     }
 
     @Test
-    public void throwsExceptionWhenDataSourceClassWithoutProperConstructorIsProvided() {
-        thrown.expect(DataSourceCreationException.class);
-        thrown.expectMessage("Unable to create instance of OntoDriver data source " + INVALID_DATA_SOURCE_CLASS);
-        new DefaultStorageAccessor(storageProperties(INVALID_DATA_SOURCE_CLASS), Collections.emptyMap());
+    void throwsExceptionWhenDataSourceClassWithoutProperConstructorIsProvided() {
+        final DataSourceCreationException ex = assertThrows(DataSourceCreationException.class,
+                () -> new DefaultStorageAccessor(storageProperties(INVALID_DATA_SOURCE_CLASS), Collections.emptyMap()));
+        assertEquals("Unable to create instance of OntoDriver data source " + INVALID_DATA_SOURCE_CLASS,
+                ex.getMessage());
     }
 
     @Test
-    public void throwsExceptionWhenUnknownClassIsSpecifiedAsDataSource() {
+    void throwsExceptionWhenUnknownClassIsSpecifiedAsDataSource() {
         final String unknownClass = "cz.cvut.kbss.jopa.UnknownDataSource";
-        thrown.expect(DataSourceCreationException.class);
-        thrown.expectMessage("Unable to find OntoDriver data source class " + unknownClass);
-        new DefaultStorageAccessor(storageProperties(unknownClass), Collections.emptyMap());
+        final DataSourceCreationException ex = assertThrows(DataSourceCreationException.class,
+                () -> new DefaultStorageAccessor(storageProperties(unknownClass), Collections.emptyMap()));
+        assertEquals("Unable to find OntoDriver data source class " + unknownClass, ex.getMessage());
     }
 
     @Test
-    public void testClose() {
+    void testClose() {
         final DefaultStorageAccessor a = new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS),
                 Collections.emptyMap());
         assertTrue(a.isOpen());
@@ -80,7 +74,7 @@ public class DefaultStorageAccessorTest {
     }
 
     @Test
-    public void closingAccessorMultipleTimesDoesNothing() {
+    void closingAccessorMultipleTimesDoesNothing() {
         final DefaultStorageAccessor a = new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS),
                 Collections.emptyMap());
         assertTrue(a.isOpen());
@@ -91,14 +85,13 @@ public class DefaultStorageAccessorTest {
     }
 
     @Test
-    public void gettingExceptionWhenTryingToAcquireConnectionThrowsStorageAccessException() throws Exception {
-        thrown.expect(StorageAccessException.class);
-        thrown.expectMessage("Unable to acquire storage connection.");
+    void gettingExceptionWhenTryingToAcquireConnectionThrowsStorageAccessException() throws Exception {
         final DefaultStorageAccessor a = new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS),
                 Collections.emptyMap());
         final DataSourceStub ds = getDataSourceStub(a);
         ds.throwExceptionOnGetConnection();
-        a.acquireConnection();
+        final StorageAccessException ex = assertThrows(StorageAccessException.class, a::acquireConnection);
+        assertEquals("Unable to acquire storage connection.", ex.getMessage());
     }
 
     private DataSourceStub getDataSourceStub(DefaultStorageAccessor a) throws NoSuchFieldException,
@@ -109,23 +102,22 @@ public class DefaultStorageAccessorTest {
     }
 
     @Test
-    public void gettingExceptionWhenTryingToCloseDataSourceThrowsStorageAccessExceptionAndClosesAccessor() throws
+    void gettingExceptionWhenTryingToCloseDataSourceThrowsStorageAccessExceptionAndClosesAccessor() throws
             Exception {
-        thrown.expect(StorageAccessException.class);
-        thrown.expectMessage("Error when closing the data source.");
         final DefaultStorageAccessor a = new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS),
                 Collections.emptyMap());
         final DataSourceStub ds = getDataSourceStub(a);
         ds.throwExceptionOnClose();
         try {
-            a.close();
+            final StorageAccessException ex = assertThrows(StorageAccessException.class, a::close);
+            assertEquals("Error when closing the data source.", ex.getMessage());
         } finally {
             assertFalse(a.isOpen());
         }
     }
 
     @Test
-    public void unwrapReturnsMatchingDataSourceInstance() {
+    void unwrapReturnsMatchingDataSourceInstance() {
         final DefaultStorageAccessor a =
                 new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS), Collections.emptyMap());
         final DataSourceStub ds = a.unwrap(DataSourceStub.class);
@@ -133,11 +125,10 @@ public class DefaultStorageAccessorTest {
     }
 
     @Test
-    public void unwrapThrowsExceptionWhenNoMatchingInstanceIsFound() {
-        thrown.expect(OWLPersistenceException.class);
-        thrown.expectMessage("Instance of class " + String.class + " not found.");
+    void unwrapThrowsExceptionWhenNoMatchingInstanceIsFound() {
         final DefaultStorageAccessor a =
                 new DefaultStorageAccessor(storageProperties(DATA_SOURCE_CLASS), Collections.emptyMap());
-        a.unwrap(String.class);
+        final OWLPersistenceException ex = assertThrows(OWLPersistenceException.class, () -> a.unwrap(String.class));
+        assertEquals("Instance of class " + String.class + " not found.", ex.getMessage());
     }
 }
--- jopa-impl/src/test/java/cz/cvut/kbss/jopa/utils/DatatypeTransformerTest.java
+++ jopa-impl/src/test/java/cz/cvut/kbss/jopa/utils/DatatypeTransformerTest.java
@@ -1,84 +1,75 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.utils;
 
 import cz.cvut.kbss.jopa.environment.OWLClassA;
 import cz.cvut.kbss.jopa.exception.UnsupportedTypeTransformation;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 import java.net.URL;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class DatatypeTransformerTest {
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
+class DatatypeTransformerTest {
 
     @Test
-    public void transformReturnsNullForNullInput() {
+    void transformReturnsNullForNullInput() {
         assertNull(DatatypeTransformer.transform(null, String.class));
     }
 
     @Test
-    public void transformConvertsValueToTargetType() {
+    void transformConvertsValueToTargetType() {
         final Integer value = 117;
         final Double result = DatatypeTransformer.transform(value, Double.class);
         assertEquals(value.doubleValue(), result, 0.01);
     }
 
     @Test
-    public void transformThrowsUnsupportedTypeConversionWhenNoTransformerIsFound() {
+    void transformThrowsUnsupportedTypeConversionWhenNoTransformerIsFound() {
         final Integer value = 117;
-        thrown.expect(UnsupportedTypeTransformation.class);
-        thrown.expectMessage(
-                String.format("Cannot transform value %s of type %s to target type %s.", value, Integer.class,
-                        URL.class));
-        DatatypeTransformer.transform(value, URL.class);
+        final UnsupportedTypeTransformation ex = assertThrows(UnsupportedTypeTransformation.class,
+                () -> DatatypeTransformer.transform(value, URL.class));
+        assertEquals(String.format("Cannot transform value %s of type %s to target type %s.", value, Integer.class,
+                URL.class), ex.getMessage());
     }
 
     @Test
-    public void transformToStringDoesNotRequireExplicitTransformer() {
+    void transformToStringDoesNotRequireExplicitTransformer() {
         final Integer value = 117;
         final String result = DatatypeTransformer.transform(value, String.class);
         assertEquals(value.toString(), result);
     }
 
     @Test
-    public void transformCastsValueWithoutTransformerWhenValueIsAssignableToTargetType() {
+    void transformCastsValueWithoutTransformerWhenValueIsAssignableToTargetType() {
         final Integer value = 117;
         final Number result = DatatypeTransformer.transform(value, Number.class);
         assertSame(result, value);
     }
 
     @Test
-    public void transformSupportsConversionUsingConstructorWithParameterMatchingTransformedValueType() {
+    void transformSupportsConversionUsingConstructorWithParameterMatchingTransformedValueType() {
         final String value = "http://onto.fel.cvut.cz";
         final URL result = DatatypeTransformer.transform(value, URL.class);
         assertEquals(value, result.toString());
     }
 
     @Test
-    public void transformThrowsUnsupportedTypeConversionWhenTargetTypeDoesNotHaveMatchingConstructor() {
+    void transformThrowsUnsupportedTypeConversionWhenTargetTypeDoesNotHaveMatchingConstructor() {
         final String value = "http://onto.fel.cvut.cz";
-        thrown.expect(UnsupportedTypeTransformation.class);
-        thrown.expectMessage(
-                String.format("Cannot transform value %s of type %s to target type %s.", value, String.class,
-                        OWLClassA.class));
-        DatatypeTransformer.transform(value, OWLClassA.class);
+        final UnsupportedTypeTransformation ex = assertThrows(UnsupportedTypeTransformation.class,
+                () -> DatatypeTransformer.transform(value, OWLClassA.class));
+        assertEquals(String.format("Cannot transform value %s of type %s to target type %s.", value, String.class,
+                OWLClassA.class), ex.getMessage());
     }
 }
