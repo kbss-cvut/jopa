843f322da2e8571373bc35a36c596b21ef09f323
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-10 09:35:00 +0200

    Minor improvement in OWLAPI SelectResultSet.

--- ontodriver-owlapi/src/main/java/cz/cvut/kbss/ontodriver/owlapi/query/SelectResultSet.java
+++ ontodriver-owlapi/src/main/java/cz/cvut/kbss/ontodriver/owlapi/query/SelectResultSet.java
@@ -1,23 +1,20 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.owlapi.query;
 
 import cz.cvut.kbss.ontodriver.Statement;
 import cz.cvut.kbss.ontodriver.exception.OntoDriverException;
 import cz.cvut.kbss.ontodriver.exception.VariableNotBoundException;
-import cz.cvut.kbss.ontodriver.model.LangString;
 import cz.cvut.kbss.ontodriver.owlapi.exception.BindingValueMismatchException;
 import cz.cvut.kbss.ontodriver.owlapi.exception.OwlapiDriverException;
 import cz.cvut.kbss.ontodriver.owlapi.util.OwlapiUtils;
@@ -290,8 +287,8 @@ class SelectResultSet extends AbstractResultSet {
             final Object ob = OwlapiUtils.owlLiteralToValue((OWLLiteral) owlValue);
             if (cls.isAssignableFrom(ob.getClass())) {
                 return cls.cast(ob);
-            } else if (String.class.equals(cls) && ob instanceof LangString) {
-                return cls.cast(((LangString) ob).getValue());
+            } else if (String.class.equals(cls)) {
+                return cls.cast(((OWLLiteral) owlValue).getLiteral());
             }
         } else {
             final Set<OWLEntity> sig = owlValue.signature().collect(Collectors.toSet());
