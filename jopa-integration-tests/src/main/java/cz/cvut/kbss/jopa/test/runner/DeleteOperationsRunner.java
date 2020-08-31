28b8506ecbf95b7a68a7dbbde24040fc94fd6c7f
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-08-31 10:10:56 +0200

    [Feature #59] Test multilingual string removal.

--- jopa-integration-tests/src/main/java/cz/cvut/kbss/jopa/test/runner/DeleteOperationsRunner.java
+++ jopa-integration-tests/src/main/java/cz/cvut/kbss/jopa/test/runner/DeleteOperationsRunner.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.test.runner;
 
@@ -19,11 +17,9 @@ import cz.cvut.kbss.jopa.model.annotations.OWLAnnotationProperty;
 import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
 import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
 import cz.cvut.kbss.jopa.test.*;
-import cz.cvut.kbss.jopa.test.environment.DataAccessor;
-import cz.cvut.kbss.jopa.test.environment.Generators;
-import cz.cvut.kbss.jopa.test.environment.PersistenceFactory;
-import cz.cvut.kbss.jopa.test.environment.TestEnvironmentUtils;
+import cz.cvut.kbss.jopa.test.environment.*;
 import cz.cvut.kbss.jopa.utils.EntityPropertiesUtils;
+import cz.cvut.kbss.jopa.vocabulary.RDF;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -593,5 +589,33 @@ public abstract class DeleteOperationsRunner extends BaseRunner {
         assertFalse(
                 em.createNativeQuery("ASK { ?x ?y ?z .}", Boolean.class).setParameter("x", URI.create(entityM.getKey()))
                   .getSingleResult());
+    }
+
+    @Test
+    void removeDeletesAllValuesOfMultilingualAttribute() throws Exception {
+        this.em = getEntityManager("removeDeletesAllValuesOfMultilingualAttribute", false);
+        final URI uri = Generators.generateUri();
+        final URI singularProperty = URI.create(Vocabulary.P_Y_SINGULAR_MULTILINGUAL_ATTRIBUTE);
+        final Map<String, String> translations = new HashMap<>();
+        translations.put("en", "building");
+        translations.put("cs", "stavba");
+        translations.put("de", "der Bau");
+        final Collection<Quad> quads = new ArrayList<>();
+        quads.add(new Quad(uri, URI.create(RDF.TYPE), URI.create(Vocabulary.C_OWL_CLASS_Y)));
+        translations.entrySet().stream().map(e -> new Quad(uri, singularProperty, e.getValue(), e.getKey())).forEach(
+                quads::add);
+        persistTestData(quads, em);
+
+        em.getTransaction().begin();
+        final OWLClassY toRemove = findRequired(OWLClassY.class, uri);
+        assertNotNull(toRemove.getSingularString());
+        assertFalse(toRemove.getSingularString().isEmpty());
+        em.remove(toRemove);
+        em.getTransaction().commit();
+
+        assertFalse(em.createNativeQuery("ASK { ?x ?singularString ?y . }", Boolean.class)
+                      .setParameter("x", toRemove.getUri())
+                      .setParameter("singularString", URI.create(Vocabulary.P_Y_SINGULAR_MULTILINGUAL_ATTRIBUTE))
+                      .getSingleResult());
     }
 }
