a640c6eb7346d2afa6f43f6c56f52c0cf96390eb
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-09-16 08:28:42 +0200

    [Enhancement #74] Create CollectionValueParameter for Collection query parameter values.

--- jopa-impl/src/test/java/cz/cvut/kbss/jopa/query/parameter/ParameterValueFactoryTest.java
+++ jopa-impl/src/test/java/cz/cvut/kbss/jopa/query/parameter/ParameterValueFactoryTest.java
@@ -1,22 +1,21 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
  * <p>
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * <p>
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.query.parameter;
 
 import cz.cvut.kbss.jopa.environment.OWLClassA;
 import cz.cvut.kbss.jopa.environment.utils.Generators;
 import cz.cvut.kbss.jopa.sessions.MetamodelProvider;
+import cz.cvut.kbss.jopa.vocabulary.XSD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -26,7 +25,10 @@ import org.mockito.MockitoAnnotations;
 import java.net.URI;
 import java.net.URL;
 import java.time.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -67,21 +69,21 @@ class ParameterValueFactoryTest {
     void createBooleanValue() {
         final ParameterValue value = sut.create(true);
         assertEquals(Boolean.TRUE, value.getValue());
-        assertEquals("\"true\"^^<http://www.w3.org/2001/XMLSchema#boolean>", value.getQueryString());
+        assertEquals("\"true\"^^<" + XSD.BOOLEAN + ">", value.getQueryString());
     }
 
     @Test
     void createShortValue() {
         final ParameterValue value = sut.create((short) 117);
         assertEquals((short) 117, value.getValue());
-        assertEquals("\"117\"^^<http://www.w3.org/2001/XMLSchema#short>", value.getQueryString());
+        assertEquals("\"117\"^^<" + XSD.SHORT + ">", value.getQueryString());
     }
 
     @Test
     void createIntegerValue() {
         final ParameterValue value = sut.create(117);
         assertEquals(117, value.getValue());
-        assertEquals("\"117\"^^<http://www.w3.org/2001/XMLSchema#int>", value.getQueryString());
+        assertEquals("\"117\"^^<" + XSD.INT + ">", value.getQueryString());
     }
 
     @Test
@@ -89,21 +91,21 @@ class ParameterValueFactoryTest {
         final long v = System.currentTimeMillis();
         final ParameterValue value = sut.create(v);
         assertEquals(v, value.getValue());
-        assertEquals("\"" + v + "\"^^<http://www.w3.org/2001/XMLSchema#long>", value.getQueryString());
+        assertEquals("\"" + v + "\"^^<" + XSD.LONG + ">", value.getQueryString());
     }
 
     @Test
     void createFloatValue() {
         final ParameterValue value = sut.create(3.14f);
         assertEquals(3.14f, value.getValue());
-        assertEquals("\"3.14\"^^<http://www.w3.org/2001/XMLSchema#float>", value.getQueryString());
+        assertEquals("\"3.14\"^^<" + XSD.FLOAT + ">", value.getQueryString());
     }
 
     @Test
     void createDoubleValue() {
         final ParameterValue value = sut.create(3.14);
         assertEquals(3.14, value.getValue());
-        assertEquals("\"3.14\"^^<http://www.w3.org/2001/XMLSchema#double>", value.getQueryString());
+        assertEquals("\"3.14\"^^<" + XSD.DOUBLE + ">", value.getQueryString());
     }
 
     @Test
@@ -111,7 +113,7 @@ class ParameterValueFactoryTest {
         final Date date = new Date();
         final ParameterValue value = sut.create(date);
         assertEquals(date, value.getValue());
-        assertEquals("\"" + date.toInstant().toString() + "\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+        assertEquals("\"" + date.toInstant().toString() + "\"^^<" + XSD.DATETIME + ">",
                 value.getQueryString());
     }
 
@@ -152,7 +154,7 @@ class ParameterValueFactoryTest {
     void createLocalDateTimeValueCreatesDateTimeParameter() {
         final LocalDateTime localDateTime = LocalDateTime.now();
         final ParameterValue value = sut.create(localDateTime);
-        assertEquals("\"" + localDateTime.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+        assertEquals("\"" + localDateTime.toString() + "\"^^<" + XSD.DATETIME + ">",
                 value.getQueryString());
     }
 
@@ -160,7 +162,7 @@ class ParameterValueFactoryTest {
     void createInstantValueCreatesDateTimeParameter() {
         final Instant instant = Instant.now();
         final ParameterValue value = sut.create(instant);
-        assertEquals("\"" + instant.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+        assertEquals("\"" + instant.toString() + "\"^^<" + XSD.DATETIME + ">",
                 value.getQueryString());
     }
 
@@ -169,7 +171,7 @@ class ParameterValueFactoryTest {
         final ZonedDateTime zonedDateTime = ZonedDateTime.now();
         final ParameterValue value = sut.create(zonedDateTime);
         assertEquals(
-                "\"" + zonedDateTime.toOffsetDateTime().toString() + "\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+                "\"" + zonedDateTime.toOffsetDateTime().toString() + "\"^^<" + XSD.DATETIME + ">",
                 value.getQueryString());
     }
 
@@ -178,7 +180,7 @@ class ParameterValueFactoryTest {
         final OffsetDateTime offsetDateTime = OffsetDateTime.now();
         final ParameterValue value = sut.create(offsetDateTime);
         assertEquals(
-                "\"" + offsetDateTime.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+                "\"" + offsetDateTime.toString() + "\"^^<" + XSD.DATETIME + ">",
                 value.getQueryString());
     }
 
@@ -186,7 +188,7 @@ class ParameterValueFactoryTest {
     void createLocalDateValueCreatesDateParameter() {
         final LocalDate localDate = LocalDate.now();
         final ParameterValue value = sut.create(localDate);
-        assertEquals("\"" + localDate.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#date>",
+        assertEquals("\"" + localDate.toString() + "\"^^<" + XSD.DATE + ">",
                 value.getQueryString());
     }
 
@@ -194,15 +196,31 @@ class ParameterValueFactoryTest {
     void createLocalTimeValueCreatesTimeParameter() {
         final LocalTime localTime = LocalTime.now();
         final ParameterValue value = sut.create(localTime);
-        assertEquals("\"" + localTime.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#time>",
+        assertEquals("\"" + localTime.toString() + "\"^^<" + XSD.TIME + ">",
                 value.getQueryString());
     }
 
     @Test
-    void createOfssetTimeValueCreatesTimeParameter() {
+    void createOffsetTimeValueCreatesTimeParameter() {
         final OffsetTime offsetTime = OffsetTime.now();
         final ParameterValue value = sut.create(offsetTime);
-        assertEquals("\"" + offsetTime.toString() + "\"^^<http://www.w3.org/2001/XMLSchema#time>",
+        assertEquals("\"" + offsetTime.toString() + "\"^^<" + XSD.TIME + ">",
                 value.getQueryString());
+    }
+
+    @Test
+    void createCollectionValueCreatesCollectionOfUriParametersValuesForUriElements() {
+        final Collection<URI> uris = Arrays
+                .asList(Generators.createIndividualIdentifier(), Generators.createIndividualIdentifier());
+        final ParameterValue result = sut.create(uris);
+        assertEquals(uris.stream().map(u -> "<" + u + ">").collect(Collectors.joining(",")), result.getQueryString());
+    }
+
+    @Test
+    void createCollectionValueCreatesCollectionOfLiteralsForLiteralElements() {
+        final Collection<Object> values = Arrays.asList(117, false);
+        final ParameterValue result = sut.create(values);
+        assertEquals("\"" + 117 + "\"^^<" + XSD.INT + ">,\"" + false + "\"^^<" + XSD.BOOLEAN + ">",
+                result.getQueryString());
     }
 }
