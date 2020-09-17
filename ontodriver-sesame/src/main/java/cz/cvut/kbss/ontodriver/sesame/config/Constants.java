bf8adba76f2a2f15470e0ac8b6512a9d4d8b1139
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-09-17 17:50:29 +0200

    [Feature #72] Add configuration constants for reconnect attempts in Sesame driver.

--- ontodriver-sesame/src/main/java/cz/cvut/kbss/ontodriver/sesame/config/Constants.java
+++ ontodriver-sesame/src/main/java/cz/cvut/kbss/ontodriver/sesame/config/Constants.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.sesame.config;
 
@@ -30,6 +28,13 @@ public class Constants {
      * without a language tag at all).
      */
     public static final String DEFAULT_LANG = null;
+
+    /**
+     * Default number of tries to connect to a remote repository.
+     *
+     * @see SesameOntoDriverProperties#SESAME_RECONNECT_ATTEMPTS
+     */
+    public static final int DEFAULT_RECONNECT_ATTEMPTS_COUNT = 5;
 
     private Constants() {
         throw new AssertionError();
