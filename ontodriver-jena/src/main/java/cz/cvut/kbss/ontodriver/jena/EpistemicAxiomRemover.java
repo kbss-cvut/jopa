6f83773600f8643dd044680c47aecb8ea6117790
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-09-24 13:00:41 +0200

    [Enhancement #73] Refactor OntoDriver API descriptors in preparation for supporting multiple contexts per assertion/subject.
    AxiomValueDescriptor has been split from AxiomDescriptor, as it can contain only one context per assertion/subject to prevent ambiguity issues when saving values.

--- ontodriver-jena/src/main/java/cz/cvut/kbss/ontodriver/jena/EpistemicAxiomRemover.java
+++ ontodriver-jena/src/main/java/cz/cvut/kbss/ontodriver/jena/EpistemicAxiomRemover.java
@@ -1,20 +1,18 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.jena;
 
-import cz.cvut.kbss.ontodriver.descriptor.AxiomDescriptor;
+import cz.cvut.kbss.ontodriver.descriptor.AbstractAxiomDescriptor;
 import cz.cvut.kbss.ontodriver.jena.connector.StorageConnector;
 import cz.cvut.kbss.ontodriver.jena.util.JenaUtils;
 import cz.cvut.kbss.ontodriver.model.Assertion;
@@ -51,12 +49,13 @@ class EpistemicAxiomRemover {
      *
      * @param descriptor Descriptor of statements to remove
      */
-    void remove(AxiomDescriptor descriptor) {
+    void remove(AbstractAxiomDescriptor descriptor) {
         final Resource subject = ResourceFactory.createResource(descriptor.getSubject().getIdentifier().toString());
         descriptor.getAssertions().stream().filter(a -> !a.isInferred()).forEach(assertion -> {
-            final URI context = descriptor.getAssertionContext(assertion);
-            final Property property = ResourceFactory.createProperty(assertion.getIdentifier().toString());
-            connector.remove(subject, property, null, context != null ? context.toString() : null);
+            descriptor.getAssertionContexts(assertion).forEach(context -> {
+                final Property property = ResourceFactory.createProperty(assertion.getIdentifier().toString());
+                connector.remove(subject, property, null, context != null ? context.toString() : null);
+            });
         });
     }
 
--- ontodriver-owlapi/src/main/java/cz/cvut/kbss/ontodriver/owlapi/EpistemicAxiomRemover.java
+++ ontodriver-owlapi/src/main/java/cz/cvut/kbss/ontodriver/owlapi/EpistemicAxiomRemover.java
@@ -1,20 +1,18 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.ontodriver.owlapi;
 
-import cz.cvut.kbss.ontodriver.descriptor.AxiomDescriptor;
+import cz.cvut.kbss.ontodriver.descriptor.AbstractAxiomDescriptor;
 import cz.cvut.kbss.ontodriver.model.Assertion;
 import cz.cvut.kbss.ontodriver.model.NamedResource;
 import cz.cvut.kbss.ontodriver.model.Value;
@@ -42,7 +40,7 @@ class EpistemicAxiomRemover {
         this.dataFactory = snapshot.getDataFactory();
     }
 
-    void remove(AxiomDescriptor descriptor) {
+    void remove(AbstractAxiomDescriptor descriptor) {
         final List<OWLOntologyChange> changes = new ArrayList<>();
         final OWLNamedIndividual individual = OwlapiUtils.getIndividual(descriptor.getSubject(), dataFactory);
         for (Assertion a : descriptor.getAssertions()) {
