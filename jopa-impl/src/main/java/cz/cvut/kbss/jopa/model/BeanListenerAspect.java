bf020e375ba83595e437d46391bd3780d985cf26
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-11-19 09:59:19 +0100

    [Bug #80] Ignore transient fields which also have property mapping in BeanListenerAspect.

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/model/BeanListenerAspect.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/model/BeanListenerAspect.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.model;
 
@@ -22,6 +20,7 @@ import cz.cvut.kbss.jopa.sessions.validator.AttributeModificationValidator;
 import cz.cvut.kbss.jopa.utils.EntityPropertiesUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.FieldSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -107,10 +106,14 @@ public class BeanListenerAspect {
         }
 
         try {
+            final Field field = ((FieldSignature) thisJoinPoint.getSignature()).getField();
+            if (EntityPropertiesUtils.isFieldTransient(field)) {
+                return;
+            }
             final FieldSpecification<?, ?> fieldSpec = getFieldSpecification(entity,
                     thisJoinPoint.getSignature().getName(), persistenceContext);
             AttributeModificationValidator.verifyCanModify(fieldSpec);
-            persistenceContext.attributeChanged(entity, fieldSpec.getJavaField());
+            persistenceContext.attributeChanged(entity, field);
         } catch (SecurityException e) {
             LOG.error(e.getMessage(), e);
             throw new OWLPersistenceException(e.getMessage());
@@ -135,9 +138,7 @@ public class BeanListenerAspect {
         if (persistenceContext == null || !persistenceContext.contains(entity)) {
             return;
         }
-        final FieldSpecification<?, ?> fieldSpec = getFieldSpecification(entity, thisJoinPoint.getSignature().getName(),
-                persistenceContext);
-        final Field field = fieldSpec.getJavaField();
+        final Field field = ((FieldSignature) thisJoinPoint.getSignature()).getField();
         if (EntityPropertiesUtils.isFieldTransient(field)) {
             return;
         }
