5a7bb9e1bbdec812910c646bd8628d2b95353c3d
Marcel Žec <marcel.zec@gmail.com>
2020-12-08 15:08:34 +0100

    rename predicate methods (equals to attrEquals etc.)

--- jopa-api/src/main/java/cz/cvut/kbss/jopa/model/query/criteria/QueryModel.java
+++ jopa-api/src/main/java/cz/cvut/kbss/jopa/model/query/criteria/QueryModel.java
@@ -2,14 +2,14 @@ package cz.cvut.kbss.jopa.model.query.criteria;
 
 // TODO PRO - QueryModel methods Javadoc
 public interface QueryModel<T> {
-    Predicate equals(Expression<?> x, Expression<?> y);
-    Predicate notEquals(Expression<?> x, Expression<?> y);
-    <Y extends Comparable<? super Y>> Predicate greaterThan(Expression<? extends Y> x, Expression<? extends Y> y);
-    <Y extends Comparable<? super Y>> Predicate greaterThan(Expression<? extends Y> x, Y y);
-    <Y extends Comparable<? super Y>> Predicate greaterThanOrEqualTo(Expression<? extends Y> x, Expression<? extends Y> y);
-    <Y extends Comparable<? super Y>> Predicate greaterThanOrEqualTo(Expression<? extends Y> x, Y y);
-    <Y extends Comparable<? super Y>> Predicate lessThan(Expression<? extends Y> x, Expression<? extends Y> y);
-    <Y extends Comparable<? super Y>> Predicate lessThan(Expression<? extends Y> x, Y y);
-    <Y extends Comparable<? super Y>> Predicate lessThanOrEqualTo(Expression<? extends Y> x, Expression<? extends Y> y);
-    <Y extends Comparable<? super Y>> Predicate lessThanOrEqualTo(Expression<? extends Y> x, Y y);
+    Predicate attrEquals(Expression<?> x, Expression<?> y);
+    Predicate attrNotEquals(Expression<?> x, Expression<?> y);
+    <Y extends Comparable<? super Y>> Predicate attrGreaterThan(Expression<? extends Y> x, Expression<? extends Y> y);
+    <Y extends Comparable<? super Y>> Predicate attrGreaterThan(Expression<? extends Y> x, Y y);
+    <Y extends Comparable<? super Y>> Predicate attrGreaterOrEqual(Expression<? extends Y> x, Expression<? extends Y> y);
+    <Y extends Comparable<? super Y>> Predicate attrGreaterOrEqual(Expression<? extends Y> x, Y y);
+    <Y extends Comparable<? super Y>> Predicate attrLessThan(Expression<? extends Y> x, Expression<? extends Y> y);
+    <Y extends Comparable<? super Y>> Predicate attrLessThan(Expression<? extends Y> x, Y y);
+    <Y extends Comparable<? super Y>> Predicate attrLessOrEqual(Expression<? extends Y> x, Expression<? extends Y> y);
+    <Y extends Comparable<? super Y>> Predicate attrLessOrEqual(Expression<? extends Y> x, Y y);
 }
