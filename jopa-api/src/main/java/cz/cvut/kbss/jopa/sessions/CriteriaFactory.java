31633b66ec99440c324ff0d38d80b14499953a46
Marcel Žec <marcel.zec@gmail.com>
2020-12-17 15:25:04 +0100

    remove <T> from CriteriaFactory interface

--- jopa-api/src/main/java/cz/cvut/kbss/jopa/sessions/CriteriaFactory.java
+++ jopa-api/src/main/java/cz/cvut/kbss/jopa/sessions/CriteriaFactory.java
@@ -3,6 +3,6 @@ package cz.cvut.kbss.jopa.sessions;
 import cz.cvut.kbss.jopa.model.query.criteria.CriteriaQuery;
 
 //TODO PRO - CriteriaFactory methods
-public interface CriteriaFactory<T> {
+public interface CriteriaFactory {
     <X> CriteriaQuery<X> from(Class<X> entityClass);
 }
