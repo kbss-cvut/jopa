e1216475867708d861eed26188c05feb3a0decde
Marcel Žec <marcel.zec@gmail.com>
2021-03-22 22:21:32 +0100

    change name of attribute in AbstractAggregateFunctionExpression

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/criteria/expressions/AbstractAggregateFunctionExpression.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/criteria/expressions/AbstractAggregateFunctionExpression.java
@@ -4,20 +4,20 @@ import cz.cvut.kbss.jopa.query.criteria.CriteriaParameterFiller;
 
 abstract public class AbstractAggregateFunctionExpression<X> extends AbstractExpression<X>{
 
-    protected AbstractPathExpression pathSource;
+    protected AbstractPathExpression internExpression;
 
-    public AbstractAggregateFunctionExpression(Class<X> type, AbstractPathExpression pathSource) {
+    public AbstractAggregateFunctionExpression(Class<X> type, AbstractPathExpression internExpression) {
         super(type);
-        this.pathSource = pathSource;
+        this.internExpression = internExpression;
     }
 
     @Override
     public void setExpressionToQuery(StringBuilder query, CriteriaParameterFiller parameterFiller) {
-        if (this.pathSource != null){
+        if (this.internExpression != null){
             throw new IllegalArgumentException("Aggregate function cannot be applied to null expression.");
         } else {
             query.append(this.getFunctionName()+"(");
-            this.pathSource.setExpressionToQuery(query, parameterFiller);
+            this.internExpression.setExpressionToQuery(query, parameterFiller);
             query.append(")");
         }
     }
