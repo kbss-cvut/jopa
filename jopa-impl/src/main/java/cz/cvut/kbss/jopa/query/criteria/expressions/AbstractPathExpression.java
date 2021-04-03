1aff03ac1fd06e1576934e6a6e36e8819df21cff
Marcel Žec <marcel.zec@gmail.com>
2021-04-03 21:43:26 +0200

    getAttr with SingularAttribute

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/criteria/expressions/AbstractPathExpression.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/criteria/expressions/AbstractPathExpression.java
@@ -17,32 +17,15 @@ abstract public class AbstractPathExpression<X> extends AbstractExpression<X> im
         this.metamodel = metamodel;
     }
 
-    //TODO - BAKALARKA - konzultacia - generika - VYRIESENE
-    // new ExpressionAttributeImpl<>
-//     Note: Applications using the string-based API may need to specify the type resulting from the get operation in order to avoid the use of Path variables.
-//     For example:
-//     CriteriaQuery<Person> q = cb.createQuery(Person.class);
-//     Root<Person> p = q.from(Person.class);
-//     q.select(p)
-//     .where(cb.isMember("joe",
-//     p.<Set<String>>get("nicknames")));
-//     rather than:
-//     CriteriaQuery<Person> q = cb.createQuery(Person.class);
-//     Root<Person> p = q.from(Person.class);
-//     Path<Set<String>> nicknames = p.get("nicknames");
-//     q.select(p)
-//     .where(cb.isMember("joe", nicknames));
     public <Y> Path<Y> getAttr(String attributeName) throws IllegalArgumentException {
         Attribute attribute = metamodel.entity(type).getAttribute(attributeName);
         Path<Y> newPathSource = new PathImpl<Y>(this.metamodel,new ExpressionAttributeImpl(attribute.getJavaType(),  this.pathSource, this.metamodel, attribute),attribute.getJavaType());
-        //TODO - BAKALARKA - konzultacia - generika - VYRIESENE
-        //new ExpressionAttributeImpl< >
         return newPathSource;
     }
 
     public <Y> Path<Y> getAttr(SingularAttribute<? super X, Y> attribute) {
-        this.pathSource = new ExpressionAttributeImpl(type,  this.pathSource, this.metamodel, attribute);
-        return this.pathSource;
+        Path<Y> newPathSource = new PathImpl<Y>(this.metamodel, new ExpressionAttributeImpl(attribute.getJavaType(),  this.pathSource, this.metamodel, attribute),attribute.getJavaType());
+        return newPathSource;
     }
 
 
