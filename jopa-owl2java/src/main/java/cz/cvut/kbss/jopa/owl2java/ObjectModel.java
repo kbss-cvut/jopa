0cec9cd2fc72e55ed1c71bc78e5f89cec8dbe680
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-11-27 18:39:24 +0100

    [Fix] Fix an issue with encoding accented characters on Windows in OWL2Java code generator.

--- jopa-owl2java/src/main/java/cz/cvut/kbss/jopa/owl2java/ObjectModel.java
+++ jopa-owl2java/src/main/java/cz/cvut/kbss/jopa/owl2java/ObjectModel.java
@@ -1,25 +1,26 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.owl2java;
 
+import com.sun.codemodel.CodeWriter;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.writer.FileCodeWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Wraps the generated object model, allowing to access it in memory or write it out to the file system.
@@ -50,7 +51,9 @@ class ObjectModel {
             if (!result && !file.exists()) {
                 LOG.error("Unable to create target directory structure.");
             }
-            codeModel.build(file);
+            // Explicitly use UTF-8 encoding to prevent issues with character encoding on different platforms
+            final CodeWriter writer = new FileCodeWriter(file, false, StandardCharsets.UTF_8.toString());
+            codeModel.build(writer);
         } catch (IOException e) {
             LOG.error("Unable to write out the generated object model.", e);
         }
