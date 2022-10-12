f68d10291dcc91b2ecbfc26ef124b2cec1145505
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-10-12 11:35:20 +0200

    [Task #75] Switch integration tests from Sesame driver to the new RDF4J driver.

---
+++ jopa-integration-tests-rdf4j/src/test/java/cz/cvut/kbss/jopa/test/query/rdf4j/QueryTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2022 Czech Technical University in Prague
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cvut.kbss.jopa.test.query.rdf4j;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.jopa.test.environment.Rdf4jPersistenceFactory;
+import cz.cvut.kbss.jopa.test.query.QueryTestEnvironment;
+import cz.cvut.kbss.jopa.test.query.runner.QueryRunner;
+import cz.cvut.kbss.ontodriver.rdf4j.config.Rdf4jOntoDriverProperties;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+public class QueryTest extends QueryRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QueryTest.class);
+
+    private static EntityManager em;
+
+    QueryTest() {
+        super(LOG);
+    }
+
+    @BeforeEach
+    void setUp() {
+        final Rdf4jPersistenceFactory persistenceFactory = new Rdf4jPersistenceFactory();
+        em = persistenceFactory.getEntityManager("SPARQLQueryTests", false,
+                Collections.singletonMap(Rdf4jOntoDriverProperties.USE_INFERENCE, "true"));
+        QueryTestEnvironment.generateTestData(em);
+        em.clear();
+        em.getEntityManagerFactory().getCache().evictAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        em.close();
+        em.getEntityManagerFactory().close();
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
---
+++ jopa-integration-tests-rdf4j/src/test/java/cz/cvut/kbss/jopa/test/query/rdf4j/SoqlTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2022 Czech Technical University in Prague
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cvut.kbss.jopa.test.query.rdf4j;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.jopa.test.environment.Rdf4jPersistenceFactory;
+import cz.cvut.kbss.jopa.test.query.QueryTestEnvironment;
+import cz.cvut.kbss.jopa.test.query.runner.SoqlRunner;
+import cz.cvut.kbss.ontodriver.rdf4j.config.Rdf4jOntoDriverProperties;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
+public class SoqlTest extends SoqlRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SoqlTest.class);
+
+    private static EntityManager em;
+
+    SoqlTest() {
+        super(LOG);
+    }
+
+    @BeforeAll
+    static void setUpBeforeClass() {
+        final Rdf4jPersistenceFactory persistenceFactory = new Rdf4jPersistenceFactory();
+        em = persistenceFactory.getEntityManager("SOQLTests", false,
+                Collections.singletonMap(Rdf4jOntoDriverProperties.USE_INFERENCE, "true"));
+        QueryTestEnvironment.generateTestData(em);
+        em.clear();
+        em.getEntityManagerFactory().getCache().evictAll();
+    }
+
+    @BeforeEach
+    void setUp() {
+        em.clear();
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() {
+        em.close();
+        em.getEntityManagerFactory().close();
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
