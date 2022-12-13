4018118220c20dba9c6238b1e26642e8067627f1
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-12-13 13:12:08 +0100

    [Enhancement #124] Skip classes annotated with NonEntity when loading entities.

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/loaders/EntityLoader.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/loaders/EntityLoader.java
@@ -1,20 +1,19 @@
 /**
  * Copyright (C) 2022 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.loaders;
 
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.util.NonEntity;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -33,7 +32,8 @@ class EntityLoader implements Consumer<Class<?>> {
 
     @Override
     public void accept(Class<?> cls) {
-        if (cls.getAnnotation(OWLClass.class) != null && !cls.isInterface()) {
+        if (cls.getAnnotation(OWLClass.class) != null
+                && cls.getAnnotation(NonEntity.class) == null && !cls.isInterface()) {
             entities.add(cls);
         }
     }
