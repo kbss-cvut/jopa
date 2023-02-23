714e1777fdd8d6466bfbe61eacf8bdff8cb6862f
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-02-23 15:19:00 +0100

    [Enhancement #136] Clear cache on EMF close.

--- jopa-impl/src/test/java/cz/cvut/kbss/jopa/model/EntityManagerFactoryImplTest.java
+++ jopa-impl/src/test/java/cz/cvut/kbss/jopa/model/EntityManagerFactoryImplTest.java
@@ -1,22 +1,22 @@
 /**
  * Copyright (C) 2022 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jopa.model;
 
 import cz.cvut.kbss.jopa.environment.OWLClassA;
 import cz.cvut.kbss.jopa.environment.utils.DataSourceStub;
 import cz.cvut.kbss.jopa.environment.utils.Generators;
+import cz.cvut.kbss.jopa.model.descriptors.EntityDescriptor;
+import cz.cvut.kbss.jopa.sessions.CacheManager;
 import cz.cvut.kbss.ontodriver.Connection;
 import cz.cvut.kbss.ontodriver.Types;
 import org.junit.jupiter.api.BeforeEach;
@@ -45,7 +45,7 @@ class EntityManagerFactoryImplTest {
         final Map<String, String> props = new HashMap<>();
         props.put(JOPAPersistenceProperties.DATA_SOURCE_CLASS, DataSourceStub.class.getName());
         props.put(JOPAPersistenceProperties.ONTOLOGY_PHYSICAL_URI_KEY,
-                Generators.createIndividualIdentifier().toString());
+                  Generators.createIndividualIdentifier().toString());
         props.put(JOPAPersistenceProperties.SCAN_PACKAGE, "cz.cvut.kbss.jopa.environment");
         this.emf = new EntityManagerFactoryImpl(props);
         emf.createEntityManager();
@@ -137,5 +137,15 @@ class EntityManagerFactoryImplTest {
     void getIdentifiersExtractsEntityIdentifier() {
         final OWLClassA instance = Generators.generateOwlClassAInstance();
         assertEquals(instance.getUri(), emf.getIdentifier(instance));
+    }
+
+    @Test
+    void closeClearsSecondLevelCache() {
+        final OWLClassA instance = Generators.generateOwlClassAInstance();
+        final CacheManager cache = emf.unwrap(CacheManager.class);
+        cache.add(instance.getUri(), instance, new EntityDescriptor());
+        assertTrue(cache.contains(OWLClassA.class, instance.getUri(), new EntityDescriptor()));
+        emf.close();
+        assertFalse(cache.contains(OWLClassA.class, instance.getUri(), new EntityDescriptor()));
     }
 }
