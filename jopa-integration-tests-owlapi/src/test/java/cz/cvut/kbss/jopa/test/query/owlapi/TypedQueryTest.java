091feb14a48aadd99a9175f01c4ff8e5dba7a0e7
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-05-05 09:49:39 +0200

    [Upd] Update to OWL2Query 0.6.2, re-enable the selection by identifier query test for OWLAPI driver.

--- jopa-integration-tests-owlapi/src/test/java/cz/cvut/kbss/jopa/test/query/owlapi/TypedQueryTest.java
+++ jopa-integration-tests-owlapi/src/test/java/cz/cvut/kbss/jopa/test/query/owlapi/TypedQueryTest.java
@@ -95,11 +95,4 @@ public class TypedQueryTest extends TypedQueryRunner {
     protected void querySupportsSelectionByDate() {
         // OWL2Query does not support filter by date and delete (used in cleanup)
     }
-
-    @Disabled
-    @Test
-    @Override
-    protected void querySupportsSelectionByEntityIdentifier() {
-        // OWL2Query does not support VALUES (it ignores them)
-    }
 }
