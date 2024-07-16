06728fe156a35e5c876973892602d7132b4b35bc
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-16 14:47:03 +0200

    [Bug #257] Add test reproducing the issue.

---
+++ modelgen/src/test/java/cz/test/ex/CustomAnnotation.java
@@ -0,0 +1,14 @@
+package cz.test.ex;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Bug #257
+ */
+@Target({ElementType.FIELD, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomAnnotation {
+}
