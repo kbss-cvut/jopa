18f4f5df7a11d921cb0c80ff58d598b79d194f19
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2025-03-11 10:52:15 +0100

    [Feature#57] Implement VirtuosoStorageConnector.

--- ontodriver-virtuoso/src/main/java/cz/cvut/kbss/ontodriver/virtuoso/VirtuosoDriverException.java
+++ ontodriver-virtuoso/src/main/java/cz/cvut/kbss/ontodriver/virtuoso/VirtuosoDriverException.java
@@ -4,11 +4,11 @@ import cz.cvut.kbss.ontodriver.exception.OntoDriverException;
 
 public class VirtuosoDriverException extends OntoDriverException {
 
-    public VirtuosoDriverException(Throwable cause) {
-        super(cause);
-    }
-
     public VirtuosoDriverException(String message) {
         super(message);
+    }
+
+    public VirtuosoDriverException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
