9def2e014485e40691df6d8ca066debda1e8d1c9
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2025-09-26 08:58:00 +0200

    [Enhancement #357] Use regular entity loading when loading from axioms fails due to IC violation.

--- jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/sparql/EntityLoadingOptimizer.java
+++ jopa-impl/src/main/java/cz/cvut/kbss/jopa/query/sparql/EntityLoadingOptimizer.java
@@ -11,6 +11,9 @@ import cz.cvut.kbss.jopa.sessions.UnitOfWork;
 
 import java.util.Optional;
 
+/**
+ * Allows optimizing queries that load entity instances.
+ */
 public class EntityLoadingOptimizer {
 
     private final UnitOfWork uow;
@@ -19,16 +22,36 @@ public class EntityLoadingOptimizer {
         this.uow = uow;
     }
 
-    public boolean canOptimize(QueryHolder query, Class<?> resultClass) {
+    /**
+     * Optionally gets a SPARQL assembly modifier that adjusts the query for optimized entity instance loading.
+     * <p>
+     * If the query cannot be optimized, an empty {@link Optional} is returned.
+     *
+     * @param query       Query holder
+     * @param resultClass Query result class
+     * @return Optional SPARQL assembly modifier
+     */
+    public Optional<SparqlAssemblyModifier> getSparqlAssemblyModifier(QueryHolder query, Class<?> resultClass) {
+        return canOptimize(query, resultClass) ? Optional.of(new EntityLoadingSparqlAssemblyModifier()) : Optional.empty();
+    }
+
+    private boolean canOptimize(QueryHolder query, Class<?> resultClass) {
         return query.getQueryType() == QueryType.SELECT && uow.isEntityType(resultClass)
                 && query.getProjectedQueryParameters().size() == 1
                 && uow.getConfiguration().is(JOPAExperimentalProperties.QUERY_ENABLE_ENTITY_LOADING_OPTIMIZER);
     }
 
-    public Optional<SparqlAssemblyModifier> getSparqlAssemblyModifier(QueryHolder query, Class<?> resultClass) {
-        return canOptimize(query, resultClass) ? Optional.of(new EntityLoadingSparqlAssemblyModifier()) : Optional.empty();
-    }
-
+    /**
+     * Gets loader of query results for the specified query.
+     * <p>
+     * If possible, a version supporting optimized entity loading is returned.
+     *
+     * @param query       Query to get loader for
+     * @param resultClass Result class
+     * @param descriptor  Descriptor specified for results
+     * @param <T>         Result type
+     * @return Query result loader
+     */
     public <T> QueryResultLoader<T> getQueryResultLoader(QueryHolder query, Class<T> resultClass,
                                                          Descriptor descriptor) {
         if (uow.isEntityType(resultClass)) {
@@ -40,6 +63,17 @@ public class EntityLoadingOptimizer {
         return new NonEntityQueryResultLoader<>(resultClass);
     }
 
+    /**
+     * Gets loader of query results for the specified query.
+     * <p>
+     * In contrast to {@link #getQueryResultLoader(QueryHolder, Class, Descriptor)} always returns the default result
+     * loader implementations.
+     *
+     * @param resultClass Result class
+     * @param descriptor  Descriptor specified for results
+     * @param <T>         Result type
+     * @return Query result loader
+     */
     public <T> QueryResultLoader<T> getUnoptimizedQueryResultLoader(Class<T> resultClass, Descriptor descriptor) {
         if (uow.isEntityType(resultClass)) {
             return new BaseEntityQueryResultLoader<>(uow, resultClass, descriptor);
